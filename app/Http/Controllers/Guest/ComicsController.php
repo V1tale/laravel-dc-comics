33e6784dd5fc602a01d84a1de65f00e354432bad
V1tale <elativ.c@gmail.com>
2022-12-21 15:47:36 +0100

    collegamenti effettuati

--- app/Http/Controllers/Guest/ComicsController.php
+++ app/Http/Controllers/Guest/ComicsController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Models\Comic;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ComicsController extends Controller
 {
-    //
+    public function index()
+    {
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
+    }
 }
